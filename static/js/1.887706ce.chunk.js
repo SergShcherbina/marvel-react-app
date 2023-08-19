df005da723e0af0d9acb5c59e31db76b9d0dc594
SergShcherbina <1990sergik27@gmail.com>
2023-08-19 17:10:08 +0300

    Updates

---
+++ static/js/1.887706ce.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 1.887706ce.chunk.js.LICENSE.txt */
+(this.webpackJsonpmarvel=this.webpackJsonpmarvel||[]).push([[1],{45:function(s,e,p){"use strict"}}]);
+//# sourceMappingURL=1.887706ce.chunk.js.map
